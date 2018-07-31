d5691cb467bedeb862e33be085493ba468ee5ab0
Cristian Mercadante <40966704+cristian-mercadante@users.noreply.github.com>
2018-07-31 23:21:30 +0200

    Create sincro.sh
    Example script to syncronize app data

---
+++ sincro.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo Sincronizzo \"mongodump_data\"
+rsync -av ~/mongodump_data ~/sincro_data/
+
+echo ------------------------
+echo Sincronizzo \"redis_data\"
+rsync -av ~/redis_data ~/sincro_data/
+
+echo ------------------------
+echo Sincronizzo \"sharelatex_data\"
+rsync -av ~/sharelatex_data ~/sincro_data/
